7b38a18fe8e8239bb070badc08060f061450561c
C-Gopal <caleb@autochartist.com>
2023-06-30 00:08:09 +0200

    First commit for poll_project

---
+++ Compulsory Task/poll_project/user_auth/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+def user_login(request):
+    return render(request, 'authentication/login.html')
+
+def authenticate_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username = username, password = password)
+
+    if user is None:
+        return HttpResponseRedirect(
+            reverse('user_auth:login')
+        )
+    else:
+        login(request, user)
+        return HttpResponseRedirect(
+            reverse('polls:polls')
+        )
+
+def show_user(request):
+    print(request.user.username)
+    return render(request, 'authentication/user.html', {
+        'username': request.user.username,
+        'password': request.user.password,
+    })
+
+def signup(request):
+    return render(request, "registration/signup.html")
