0d38b6cc8da19be846cb559937fbc77c9436bed9
C-Gopal <caleb@autochartist.com>
2023-06-30 00:28:39 +0200

    Added docstrings to functions in polls/views.py

--- Compulsory Task/poll_project/polls/views.py
+++ Compulsory Task/poll_project/polls/views.py
@@ -6,22 +6,27 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
+    """Welcome page"""
     return render(request, "welcome.html")
 
 def polls(request):
+    """Displays latest 5 polls in descending order according to publish date"""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/poll.html", context)
 
 def detail(request, question_id):
+    """Displays the details of a selected poll"""
     question = get_object_or_404(Question, pk = question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
+    """Displays results for selected poll"""
     question = get_object_or_404(Question, pk = question_id)
     return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
+    """Allows user to vote on poll options"""
     question = get_object_or_404(Question, pk = question_id)
     try:
         selected_choice = question.choice_set.get(
